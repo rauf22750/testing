682c890a7c9fa1949d7052b77ff438a8e9954b2d
Abdul Rauf <rauf22750@gmail.com>
2025-01-29 14:46:12 +0500

    first commit

--- testing/views.py
+++ testing/views.py
@@ -1,9 +1,23 @@
 from django.http import HttpResponse
+from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, World!")
+    # Define the HTML content
+    html_content = """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Welcome</title>
+    </head>
+    <body>
+        <h1>Hello, World!</h1>
+        <p>This is a simple HTML page returned from the index view in Django.</p>
+    </body>
+    </html>
+    """
+    
+    # Return the HTML content as an HTTP response
+    return HttpResponse(html_content)
 
-from django.shortcuts import render
-
-def home(request):
-    return render(request, 'homepage/home.html')
